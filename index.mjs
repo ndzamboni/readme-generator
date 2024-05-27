698efb3389ca3c47da55149dc3d5b33aec8767a8
ndzamboni <ndzamboni@gmail.com>
2024-05-26 21:52:21 -0400

    script working

---
+++ index.mjs
@@ -0,0 +1,98 @@
+import inquirer from 'inquirer';
+import fs from 'fs';
+
+// Questions array for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPLv2', 'Apache', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
+
+// Function to generate README content
+const generateREADME = (answers) => `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+For any questions, please contact me at [${answers.email}](mailto:${answers.email}). You can also find me on GitHub at [${answers.github}](https://github.com/${answers.github}).
+`;
+
+// Function to initialize program
+const init = () => {
+  inquirer.prompt(questions).then((answers) => {
+    const readmeContent = generateREADME(answers);
+    fs.writeFileSync('README.md', readmeContent);
+    console.log('README.md has been generated!');
+  });
+};
+
+// Initialize the program
+init();
